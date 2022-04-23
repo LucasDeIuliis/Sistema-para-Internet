36283a11cc88b637407d3cf8b9e13c6fd922c057
Lucas De Iuliis <87149912+LucasDeIuliis@users.noreply.github.com>
2022-04-23 17:03:44 +0000

    Começo

---
+++ Entrada e saída.js
@@ -0,0 +1,19 @@
+//saida de dados
+
+alert("my first program!")   
+console.log("test it!")
+
+//entrada de dados
+
+/*nome = prompt("digite seu nome: ")
+sobrenome = prompt('digite seu sobre nome: ')
+
+alert(nome + ' ' + sobrenome)*/
+remetente = prompt ("qual seu nome: ")
+destinatario = prompt ("qual o destinatario: ")
+msg = prompt("qual sera sua mensagem: ")
+
+alert ("Remetente: " + remetente)
+alert ("Destinatário: " + destinatario)
+alert ("Mensagem: " + msg)
+
